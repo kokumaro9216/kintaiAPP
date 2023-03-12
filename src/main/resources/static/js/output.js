abe8f3c9dee63992617637b6f70fef46625447c1
rsakata-0303 <sakataryohei.rpc@gmail.com>
2023-03-12 17:14:44 +0900

    出力追加

--- src/main/resources/static/js/output.js
+++ src/main/resources/static/js/output.js
@@ -1,7 +1,8 @@
-const start = document.getElementById('start');
-const end = document.getElementById('end');
+var start = document.getElementById('start');
+var end = document.getElementById('end');
 let checkButton = document.getElementById('checkButton');
 checkButton.addEventListener('click', bottunClick);
+
 
 function diffMonth(d1, d2){
 	let d1Month, d2Month;
@@ -14,25 +15,39 @@ function diffMonth(d1, d2){
 
 function bottunClick(){
 	msg.innerText = '開始 : ' + start.value + ' 終了 : ' + end.value + '期間 : ' + diffMonth(start.value, end.value);
+	var endValue = end.value;
+	var endYear = endValue.substr(0,4)
+	var endMonth = endValue.substr(5)
+	var startYear = start.value.substr( 0, 4 )
+	var startMonth = start.value.substr( 5 )
+	var startDate = new Date(startYear, startMonth -1, "01")
+	var endDate = new Date(endYear, endMonth, "01") -1
+	var array = getDBData(startDate, endDate);
 	var sumWorkTime = document.getElementById("sumWorkTime");
-	sumWorkTime.innerHTML = getSumWorkTime();
+	sumWorkTime.innerHTML = getSumWorkTime(array);
 	var averageWorkTime = document.getElementById("averageWorkTime");
-	averageWorkTime.innerHTML = getAverageWorkTime();
+	averageWorkTime.innerHTML = getAverageWorkTime(array);
 	var sumOvertimeWorkTime = document.getElementById("sumOvertimeWorkTime");
-	sumOvertimeWorkTime.innerHTML = getSumOvertimeWorkTime();
+	sumOvertimeWorkTime.innerHTML = getSumOvertimeWorkTime(array);
 	var averageOvertimeWorkTime = document.getElementById("averageOvertimeWorkTime");
-	averageOvertimeWorkTime.innerHTML = getAverageOvertimeWorkTime();
+	averageOvertimeWorkTime.innerHTML = getAverageOvertimeWorkTime(array);
+	generate_table(array);
   }
 
-var date1 = new Date(2022, 12, 30, 9, 00, 00);
-var date2 = new Date(2022, 12, 30, 18, 00, 00);
-var rest = 1
-var date3 = new Date(2022, 12, 31, 9, 00, 00);
-var date4 = new Date(2022, 12, 31, 18, 00, 00);
-var array = [[date1, date2, rest], [date3, date4, rest]]
+  //DBから入力された期間の出勤情報を取得する
+  function getDBData(startDate, endDate){
+	var date1 = new Date(2022, 12, 30, 9, 00, 00);
+	var date2 = new Date(2022, 12, 30, 18, 00, 00);
+	var rest = 1
+	var bikou1 = "備考1"
+	var date3 = new Date(2022, 12, 31, 9, 00, 00);
+	var date4 = new Date(2022, 12, 31, 18, 00, 00);
+	var bikou2 = "備考2"
+	var array = [[date1, date2, rest, bikou1], [date3, date4, rest, bikou2]]
+	return array
+  }
 
-
-function getSumWorkTime(){
+function getSumWorkTime(array){
 	var sum = 0
 	for(i=0; i<array.length; i++){
 		sum = sum + array[i][1].getTime()-array[i][0].getTime()-array[i][2]*60*60*1000
@@ -40,12 +55,12 @@ function getSumWorkTime(){
 	return sum/(1000 * 60 * 60)
 }
 
-function getAverageWorkTime(){
-	var sum = getSumWorkTime();
+function getAverageWorkTime(array){
+	var sum = getSumWorkTime(array);
 	return sum/(diffMonth(start.value, end.value))
 }
 
-function getSumOvertimeWorkTime(){
+function getSumOvertimeWorkTime(array){
 	var sum = 0;
 	for(i=0; i<array.length; i++){
 		overtimeworkPerDay = array[i][1].getTime()-array[i][0].getTime()-array[i][2]*60*60*1000-1000*60*60*9
@@ -56,7 +71,80 @@ function getSumOvertimeWorkTime(){
 	return sum;
 }
 
-function getAverageOvertimeWorkTime(){
-	var sum = getSumOvertimeWorkTime();
+function getAverageOvertimeWorkTime(array){
+	var sum = getSumOvertimeWorkTime(array);
 	return sum/(diffMonth(start.value, end.value)*60*60*1000)
 }
+
+function generate_table(array) {
+	// body の参照を取得
+	var body = document.getElementsByTagName("body")[0];
+  
+	// <table> 要素と <tbody> 要素を作成
+	var tbl = document.createElement("table");
+	var tblBody = document.createElement("tbody");
+	var row = document.createElement("tr");
+	var cell = document.createElement("td");
+		var cellText = document.createTextNode("日時");
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode("出勤時間");
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode("退勤時間");
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode("休憩時間");
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode("備考");
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+  
+	  // 表の本体の末尾に行を追加
+	  tblBody.appendChild(row);
+  
+	// すべてのセルを作成
+	array.forEach (function(arrayElement) {
+	  // 表の行を作成
+	  var row = document.createElement("tr");
+	  var formattedDate = `
+	  ${arrayElement[0].getFullYear()}-
+	  ${arrayElement[0].getMonth()+1}-
+	  ${arrayElement[0].getDate()}
+	  `.replace(/\n|\r/g, '');
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode(formattedDate);
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode(arrayElement[0].getHours()+":"+arrayElement[0].getMinutes());
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode(arrayElement[1].getHours()+":"+arrayElement[1].getMinutes());
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode(arrayElement[2]);
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+		var cell = document.createElement("td");
+		var cellText = document.createTextNode(arrayElement[3]);
+		cell.appendChild(cellText);
+		row.appendChild(cell);
+  
+	  // 表の本体の末尾に行を追加
+	  tblBody.appendChild(row);
+	});
+  
+	// <tbody> を <table> の中に追加
+	tbl.appendChild(tblBody);
+	// <table> を <body> の中に追加
+	body.appendChild(tbl);
+	tbl.setAttribute("border", "1");
+  }
