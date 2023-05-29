f28f3613f0203b8b0b4340a0623ee0ecc7d6bc18
miyah <miyah@192.168.11.9>
2023-05-29 20:31:01 +0900

    oauth2認証(動かない)  ※gitからtokenは取得できるようになったけど、リダイレクトを延々繰り返す挙動は変わってない

--- src/main/java/com/example/kintaiapp/controller/MainController.java
+++ src/main/java/com/example/kintaiapp/controller/MainController.java
@@ -1,57 +1,108 @@
 package com.example.kintaiapp.controller;
 
-import java.util.Collections;
+import java.net.URI;
 import java.util.Map;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Controller
 public class MainController {
-    @RequestMapping("/home")
-    public String home() {
-        return "home";
+	/*@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "input";
+	}
+	
+	@RequestMapping("/logout")
+	public String logout() {
+		return "home";
+	}
+	
+	@RequestMapping("/input")
+	public String input() {
+		return "input";
+	}
+	
+	@RequestMapping("/output")
+	public String output() {
+	    return "output";
+	}
+	
+	@RequestMapping("/account")
+	public String account() {
+	    return "account-entry";
+	}
+	
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+	    return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+	
+	public static void main(String[] args) {
+	    SpringApplication.run(MainController.class, args);
+	}*/
+
+
+    private final RestOperations restOperations = new RestTemplate();
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    // 認可済みのクライアント情報(クライアント情報、認可したリソースオーナ名、アクセストークンなど)は
+    // OAuth2AuthorizedClientService経由で取得できる
+    public MainController(OAuth2AuthorizedClientService authorizedClientService) {
+      this.authorizedClientService = authorizedClientService;
     }
 
-    @RequestMapping("/login")
-    public String login() {
-        return "input";
+    @GetMapping("/")
+    public String index(OAuth2AuthenticationToken authentication, Model model) {
+      // 画面に表示するために、OAuth2AuthorizedClientService経由で認可済みのクライアント情報を取得しModelに格納
+      model.addAttribute("authorizedClient", this.getAuthorizedClient(authentication));
+      return "index";
     }
 
-    @RequestMapping("/logout")
-    public String logout() {
-        return "home";
+    @GetMapping("/attributes")
+    public String userAttributeAtLogin(@AuthenticationPrincipal OAuth2User oauth2User, Model model) {
+      // 認証が制した時点のユーザ属性であれば、プロバイダに問い合わせなくても認証情報から取得できる。
+      // 注：あくまで認証時点での情報なので、情報は古くなっている可能性がある
+      model.addAttribute("attributes", oauth2User.getAttributes());
+      return "userinfo";
     }
 
-    @RequestMapping("/input")
-    public String input() {
-        return "input";
+    @GetMapping("/attributes/latest")
+    public String userLatestAttribute(OAuth2AuthenticationToken authentication, Model model) {
+      // 最新のユーザ属性が必要な場合は、認証時に取得したアクセストークンを付与してプロバイダから再取得する
+      // ここでは、Spring Framework提供のRestTemplateを使用してユーザ属性の取得を行っている。
+      // ちなみに・・・Spring Securityのデフォルト実装では、「Nimbus OAuth 2.0 SDK」を使用してアクセストークン及びユーザ属性の取得を行っている。
+      OAuth2AuthorizedClient authorizedClient = this.getAuthorizedClient(authentication);
+      String userInfoUri = authorizedClient.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri();
+      RequestEntity<Void> requestEntity = RequestEntity.get(URI.create(userInfoUri))
+          .header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue())
+          .build();
+      model.addAttribute("attributes", restOperations.exchange(requestEntity, Map.class).getBody());
+      return "userinfo";
     }
 
-    @RequestMapping("/output")
-    public String output() {
-        return "output";
+    private OAuth2AuthorizedClient getAuthorizedClient(OAuth2AuthenticationToken authentication) {
+      return this.authorizedClientService.loadAuthorizedClient(
+          authentication.getAuthorizedClientRegistrationId(), authentication.getName());
     }
 
-    @RequestMapping("/account")
-    public String account() {
-        return "account-entry";
-    }
-    
-    @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(MainController.class, args);
-    }
-    
  
 }
--- src/main/java/com/example/kintaiapp/security/SecurityConfig.java
+++ src/main/java/com/example/kintaiapp/security/SecurityConfig.java
@@ -1,61 +1,66 @@
 package com.example.kintaiapp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.kintaiapp.service.CustomOAuth2UserService;
 
 @Configuration
 @EnableWebSecurity
 @ComponentScan("com.example.kintaiapp.service")
 public class SecurityConfig {
 
-    @Autowired
-    UserDetailsService userDetailsService;
+	@Autowired
+	UserDetailsService userDetailsService;
 
-    // ***パスワードエンコーダーを使う場合は①と②をコメントアウトする***
-    // ①パスワードのエンコーダー
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    // return new BCryptPasswordEncoder();
-    // }
+	@Autowired
+	CustomOAuth2UserService customOAuth2UserService;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        // ***認証にUserDetailServiceを使う
-        auth.userDetailsService(userDetailsService);
-        // ②
-        // auth.userDetailsService(userDetailsService);
-        // .passwordEncoder(passwordEncoder());
-    }
+	// ***パスワードエンコーダーを使う場合は①と②をコメントアウトする***
+	// ①パスワードのエンコーダー
+	// @Bean
+	// public PasswordEncoder passwordEncoder() {
+	// return new BCryptPasswordEncoder();
+	// }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.formLogin(login -> login
-                .loginProcessingUrl("/login")
-                .loginPage("/home")
-                .defaultSuccessUrl("/input", true)
-                .usernameParameter("userid")
-                .passwordParameter("password")
-                .failureUrl("/home?error")
-                .permitAll())
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/home"))
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/","/home", "/account", "/account-entry.html").permitAll()
-                        .requestMatchers("/css/**", "/js/**").permitAll()
-                        .anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2
-                		.loginPage("/home")
-                		.defaultSuccessUrl("/input", true)
-                		.permitAll()
-                		);
-        return http.build();
-    }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+			throws Exception {
+		// ***認証にUserDetailServiceを使う
+		auth.userDetailsService(userDetailsService);
+		// ②
+		// auth.userDetailsService(userDetailsService);
+		// .passwordEncoder(passwordEncoder());
+	}
+
+	/*@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.formLogin(login -> login
+				.loginProcessingUrl("/login")
+				.loginPage("/home")
+				.defaultSuccessUrl("/input", true)
+				.usernameParameter("userid")
+				.passwordParameter("password")
+				.failureUrl("/home?error")
+				.permitAll())
+				.logout(logout -> logout
+						.logoutSuccessUrl("/home"))
+				.authorizeHttpRequests(authz -> authz
+						.requestMatchers("/", "/home", "/account", "/account-entry.html").permitAll()
+						.requestMatchers("/css/**", "/js/**").permitAll()
+						.anyRequest().authenticated())
+				.oauth2Login(oauth2 -> oauth2
+						.loginPage("/home")
+						.defaultSuccessUrl("/input", true)
+						.failureUrl("/home?erroroauth2")
+						.permitAll()
+						.userInfoEndpoint(userInfo -> userInfo
+								.userService(customOAuth2UserService)));
+		return http.build();
+	}
+	*/
 }
